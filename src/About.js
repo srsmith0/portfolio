fa5bf093d71e081612ab7c474c4e0841f3268615
Shawn Smith <srsmith11@live.com>
2020-09-08 14:48:08 -0600

    mvp

--- src/About.js
+++ src/About.js
@@ -17,9 +17,9 @@ function About(props) {
 							attended the University of Utah/DevPoint Lab’s Web Development Bootcamp in May 2020. I was
 							in the brewing industry for six years but decided to change careers. I have a passion for
 							creating things for people to enjoy, no matter the medium and software development fits
-							perfectly into this passion. I began teaching myself web development in January 2020 and
-							immediately enjoyed the challenge and creativity that programming offers. Brewing gave me a
-							continous improvement mindset and I improve, learn, and create new things every day.
+							perfectly into this passion. I began teaching myself web development in January 2020 and was
+							immediately drawn to the challenge and creativity that programming offers. Brewing taught me
+							a continous improvement mindset and I improve, learn, and create new things every day.
 						</p>
 					</div>
 				</div>
