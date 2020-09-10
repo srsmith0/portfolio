7f1d146f6705950239a346d6c7b04b72219b500e
Shawn Smith <srsmith11@live.com>
2020-09-10 12:40:47 -0600

    added dnd chars to projects

--- src/Projects.js
+++ src/Projects.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Thumbnail from './Thumbnail';
 import l3 from './L3.jpeg';
 import familyTalk from './familytalk.jpeg';
+import dnd from './dndchars.png';
 
 function Projects(props) {
 	const l3Description =
-		'Live, Laugh, Lager is an app for reviewing beers, making beer related posts, following friends, and finding breweries. I always found it incredibly frustrating as a brewer that other beer rating apps only required an overall rating for a beer score.  So I require a user to rate 4 critera and the app creates the overall score.  The most challenging part of the project was using a third party API call.  I complete this call in the backend, something that I never had done before. It was made with React.js and Rails.';
+		'Live, Laugh, Lager is an app for reviewing beers, making beer related posts, following friends, and finding breweries. I always found it incredibly frustrating as a brewer that other beer rating apps only required an overall rating for a beer score.  So I require a user to rate 4 critera and the app creates the overall score.  The most challenging part of the project was using a third party API call.  I complete this call in the backend, something that I never had done before. It was created with React.js and Rails.';
+
+	const dndDescription =
+		"D&D Database started as a Hackathon project that I then polished. I did all of the SQL programming to display a Dungeons and Dragons character information during the hackathon and then created a simple webpage to navigate through the database's seeded information. I have included a link to the database diagram as well. It was created with React.js, SQL, and Rails";
 
 	const familyTalkDescription =
-		'Family Talk allows users to create boards with a specific topic, create posts about that topic within the board, has the ability to like and comment on posts, and an option to follow boards. This was a group project through the University of Utah/DevPoint Labs. I created the comment components, the like functionality, and the follow board functionality. It was made with React.js and Rails.';
+		'Family Talk allows users to create boards with a specific topic, create posts about that topic within the board, has the ability to like and comment on posts, and an option to follow boards. This was a group project through the University of Utah/DevPoint Labs. I created the comment components, the like functionality, and the follow board functionality. It was created with React.js and Rails.';
 	return (
 		<div>
 			<h1 className="project-page-title">Projects</h1>
@@ -18,6 +22,13 @@ function Projects(props) {
 				title="Live, Laugh, Lager"
 				code="https://github.com/srsmith0/live-laugh-lager"
 				description={l3Description}
+			/>
+			<Thumbnail
+				deploy="https://dnd-characters-db.herokuapp.com/"
+				image={dnd}
+				title="D&D Database"
+				code="https://github.com/srsmith0/dnd"
+				description={dndDescription}
 			/>
 			<Thumbnail
 				deploy="https://family-talk.herokuapp.com/"
