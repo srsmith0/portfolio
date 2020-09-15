e1457ca988d2aeac764abf46e2d42377c3816ced
Shawn Smith <srsmith11@live.com>
2020-09-15 17:44:48 -0600

    changed skills section

--- src/About.js
+++ src/About.js
@@ -7,39 +7,51 @@ function About(props) {
 		<div>
 			<h1 className="about-title">About Me</h1>
 			<div className="about-section">
-				<div className="self-image">
+				<figure className="self-image">
 					<img src={self} alt="Self" />
-				</div>
+				</figure>
 				<div className="about-description">
-					<div className="description-sub">
-						<p>
-							My name is Shawn Smith and I am a Full Stack Developer based in Salt Lake City, UT. I
-							attended the University of Utah/DevPoint Lab’s Web Development Bootcamp in May 2020. I was
-							in the brewing industry for six years but decided to change careers. I have a passion for
-							creating things for people to enjoy, no matter the medium and software development fits
-							perfectly into this passion. I began teaching myself web development in January 2020 and was
-							immediately drawn to the challenge and creativity that programming offers. Brewing taught me
-							a continous improvement mindset and I improve, learn, and create new things every day.
-						</p>
-					</div>
+					<p>
+						My name is Shawn Smith and I am a Full Stack Developer based in Salt Lake City, UT. I attended
+						the University of Utah/DevPoint Lab’s Web Development Bootcamp in May 2020. I was in the brewing
+						industry for six years but decided to change careers. I have a passion for creating things for
+						people to enjoy, no matter the medium and software development fits perfectly into this passion.
+						I began teaching myself web development in January 2020 and was immediately drawn to the
+						challenge and creativity that programming offers. Brewing taught me a continous improvement
+						mindset and I improve, learn, and create new things every day.
+					</p>
 				</div>
 			</div>
+			<h2 className="skills-title">Skills</h2>
+			<div className="about-icons">
+				<Icon name="js square" color="yellow" size="big" />
+				<Icon name="html5" color="red" size="big" />
+				<Icon name="css3 alternate" color="blue" size="big" />
+				<Icon name="react" color="blue" size="big" />
+				<Icon name="gem" color="red" size="big" />
+			</div>
 			<div className="skills">
-				<h3 className="skills-title">My Skills</h3>
-				<div className="skills-list">
+				<div className="skills-card">
+					<h3 className="skills-card-title">Front End</h3>
+					<hr />
 					<p>JavaScript</p>
-					<p>HTML</p>
-					<p>CSS</p>
 					<p>React.js</p>
-					<p>Rails</p>
-					<p>SQL</p>
+					<p>HTML</p>
+					<p>CSS/Sass</p>
 				</div>
-				<div className="about-icons">
-					<Icon name="js square" color="yellow" size="huge" />
-					<Icon name="html5" color="red" size="huge" />
-					<Icon name="css3 alternate" color="blue" size="huge" />
-					<Icon name="react" color="blue" size="huge" />
-					<Icon name="gem" color="red" size="huge" />
+				<div className="skills-card">
+					<h3 className="skills-card-title">Back End</h3>
+					<hr />
+					<p>Rails</p>
+					<p>SQL/PostgreSQL</p>
+				</div>
+				<div className="skills-card">
+					<h3 className="skills-card-title">Other</h3>
+					<hr />
+					<p>Git/GitHub</p>
+					<p>Agile</p>
+					<p>Chrome developer tools</p>
+					<p>Heroku/Netlify</p>
 				</div>
 			</div>
 		</div>
