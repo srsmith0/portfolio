b4f770d8f0304216408f2b0f3c699e9c248bc2ec
Shawn Smith <srsmith11@live.com>
2021-08-23 21:27:59 -0600

    updated with ultra planner

--- src/About.js
+++ src/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import self from './self.png';
 
 function About() {
 	return (
@@ -9,12 +8,12 @@ function About() {
 				<div className="about-description">
 					<p>
 						My name is Shawn Smith and I am a Full Stack Developer based in Salt Lake City, UT. I attended
-						the University of Utah/DevPoint Lab’s Web Development Bootcamp in May 2020. I was in the brewing
-						industry for six years but decided to change careers. I have a passion for creating things for
-						people to enjoy, no matter the medium and software development fits perfectly into this passion.
+						the University of Utah/DevPoint Lab’s Web Development Bootcamp in May 2020.  I have a passion for creating things for
+						people to enjoy, no matter the medium, and software development fits perfectly into this passion.
 						I began teaching myself web development in January 2020 and was immediately drawn to the
-						challenge and creativity that programming offers. Brewing taught me a continous improvement
-						mindset and I improve, learn, and create new things every day.
+						challenge and creativity that programming offers. I have worked in the beer brewing industry for 7 years
+						and that time has instilled a continous improvement mindset in me.  I constantly want to improve, learn,
+						and create new things every day.
 					</p>
 				</div>
 			</div>
